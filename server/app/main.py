88ae50bad88077646f6e946fbd387f6f1ee96c52
shravya-chepa <riya.chepa@gmail.com>
2024-12-09 02:25:38 -0800

    add cors middleware in server

--- server/app/main.py
+++ server/app/main.py
@@ -1,8 +1,18 @@
 from fastapi import FastAPI
 from app.routes import book_router
 from app.auth.auth_routes import auth_router
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
+
+# Add CORS middleware
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  # Adjust this to restrict domains in production
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 # Include the API routes
 app.include_router(book_router)
